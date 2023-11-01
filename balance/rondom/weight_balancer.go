8fdc61d7283eb7e383f44096383b7dd758468ee6
Ai-Feier <2664586998@qq.com>
2023-11-01 17:16:28 +0800

    micro:新增grpc加权随机负载均衡算法

---
+++ balance/rondom/weight_balancer.go
@@ -0,0 +1,62 @@
+package rondom
+
+import (
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+	"math/rand"
+)
+
+type WeightBalancer struct {
+	connections []*weightConn
+	totalWeight uint32
+}
+
+func (b *WeightBalancer) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+	if len(b.connections) == 0 {
+		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+	}
+	tgt := rand.Intn(int(b.totalWeight) + 1)
+	var idx int
+	for i, c := range b.connections {
+		tgt -= int(c.weight)
+		if tgt <= 0 {
+			idx = i
+			break
+		}
+	}
+	return balancer.PickResult{
+		SubConn: b.connections[idx].c,
+		Done: func(info balancer.DoneInfo) {
+			// 可根据 info 是否存在错误, 修改权重
+		},
+	}, nil
+}
+
+type WeightBalancerBuilder struct {
+
+}
+
+func (b *WeightBalancerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
+	cs := make([]*weightConn, 0, len(info.ReadySCs))
+	var totalWeight uint32
+	for sub, subInfo := range info.ReadySCs {
+		weight := subInfo.Address.Attributes.Value("weight").(uint32)
+		totalWeight += weight
+		cs = append(cs, &weightConn{
+			c: sub,
+			weight: weight,
+		})
+	}
+	return &WeightBalancer{
+		connections: cs,
+		totalWeight: totalWeight,
+	}
+}
+
+
+type weightConn struct {
+	c balancer.SubConn
+	weight uint32
+}
+
+
